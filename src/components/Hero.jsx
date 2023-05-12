331bdf59d3a9f7b2f71dc140ce4335df3499f0c5
Josh Mason <josh.mason.music@gmail.com>
2023-05-12 16:30:02 +1200

    WIP Computers component for 3D bit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,8 +10,21 @@ const Hero = () => {
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         {' '}
-        <div className="flex flex-col justify-center items-center mt-5"></div>
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Kia ora, I&apos;m <span className="text-[#915eff]">Josh</span>
+          </h1>
+          <p className={`{styles.heroSubText} mt-2 text-white-100`}>
+            I make web apps, music <br className="sm:block hidden" /> and write
+            things.
+          </p>
+        </div>
       </div>
+      <ComputersCanvas />
     </section>
   )
 }
