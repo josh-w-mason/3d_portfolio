0925550b8e70987fa8a791229a32b8bb25b25557
Josh Mason <josh.mason.music@gmail.com>
2023-05-15 18:04:38 +1200

    fix contact form template key

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,14 +7,56 @@ import { EarthCanvas } from './canvas'
 import { SectionWrapper } from '../hoc'
 import { slideIn } from '../utils/motion'
 
+//
+//
+//
+
 const Contact = () => {
   const formRef = useRef()
   const [form, setForm] = useState({ name: '', email: '', message: '' })
   const [loading, setLoading] = useState(false)
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target
 
-  const handleSubmit = (e) => {}
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setLoading(true)
+    console.log('submit clicked')
+    emailjs
+      .send(
+        'service_g9qwwal',
+        'template_rpvqwbb',
+        {
+          from_name: form.name,
+          to_name: 'Josh',
+          from_email: form.email,
+          to_email: 'josh.mason.music@gmail.com',
+          message: form.message,
+        },
+        '79tWGTmORcIAKH_Fq'
+      )
+      .then(
+        () => {
+          setLoading(false)
+          alert('Thank you. I will get back to you as soon as possible.')
+
+          setForm({
+            name: '',
+            email: '',
+            message: '',
+          })
+        },
+        (error) => {
+          setLoading(false)
+          console.log(error)
+          alert('Something went wrong')
+        }
+      )
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
