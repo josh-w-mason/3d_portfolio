166848d68e1348a961287140cd8d2116c4d0aa76
Josh Mason <josh.mason.music@gmail.com>
2023-05-15 16:17:30 +1200

    WIP add 3D earth model to contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -23,7 +23,59 @@ const Contact = () => {
         className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
       >
         <p className={styles.sectionSubText}>Get in touch</p>
-        <h3 className={styles.sectionHeadText}>Contact</h3>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What is your name?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondar text-white rounded-lg outlined-none border-none font-medium"
+            ></input>
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your email</span>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What is your email?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondar text-white rounded-lg outlined-none border-none font-medium"
+            ></input>
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your message</span>
+            <textarea
+              rows="7"
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="What do you want to say?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondar text-white rounded-lg outlined-none border-none font-medium"
+            ></textarea>
+          </label>
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
+          >
+            {loading ? 'Sending...' : 'send'}
+          </button>
+        </form>
+      </motion.div>
+      <motion.div
+        variants={slideIn('right', 'tween', 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
       </motion.div>
     </div>
   )
--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+
+import CanvasLoader from '../Loader'
 
 const Earth = () => {
+  const earth = useGLTF('./planet/scene.gltf')
+
+  return <primitive object={earth.scene} scale={2.5} />
+}
+
+const EarthCanvas = () => {
   return (
-    <div>Earth</div>
+    <Canvas
+      shadows
+      frameloop="demand"
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{}}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        >
+          <Earth />
+        </OrbitControls>
+      </Suspense>
+    </Canvas>
   )
 }
 
-export default Earth
+export default EarthCanvas
