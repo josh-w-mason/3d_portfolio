b89a3222836c1d639e9daebc3d6d3f54c545ca1a
Josh Mason <josh.mason.music@gmail.com>
2023-05-19 13:29:54 +1200

    fix 3d model for mobile view

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -20,8 +20,8 @@ const Mainanimation = ({ isMobile }) => {
       />
       <primitive
         object={bird.scene}
-        scale={isMobile ? 0.7 : 0.01}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.005 : 0.01}
+        position={isMobile ? [0, -3, -0.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
