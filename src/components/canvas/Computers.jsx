7fe03ba718a2d136292b28caf36e834ad190b0cc
Josh Mason <josh.mason.music@gmail.com>
2023-05-23 14:28:32 +1200

    remove line 53 in Computers.jsx for property gl={{ preserveDrawingBuffer: true }} to see if fixes mobile viewing issue for front page

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -50,7 +50,6 @@ const MainanimationCanvas = () => {
       frameloop="demand"
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
