654bce215e4693357d18045aa32a15c3e14d59f4
Josh Mason <josh.mason.music@gmail.com>
2023-06-20 11:52:53 +1200

    revert 3d model and settings in Computers.jsx to check if bug resolved on phone display

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,24 +4,24 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
 const Mainanimation = ({ isMobile }) => {
-  const bird = useGLTF('./the_neko/scene.gltf')
+  const computer = useGLTF('./desktop_pc/scene.gltf')
 
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
-      <pointLight intensity={1} />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
         castShadow
-        shadow-mapsize={1024}
+        shadow-mapSize={1024}
       />
+      <pointLight intensity={1} />
       <primitive
-        object={bird.scene}
-        scale={isMobile ? 0.005 : 0.01}
-        position={isMobile ? [0, -3, -0.2] : [0, -3.25, -1.5]}
+        object={computer.scene}
+        scale={isMobile ? 0.6 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -49,7 +49,9 @@ const MainanimationCanvas = () => {
     <Canvas
       frameloop="demand"
       shadows
+      dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
